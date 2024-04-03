3d0257f411a6f175b02ed49a757a9f5fe9a77533
georgiasantos-frontend <georgiasantos.frontend@gmail.com>
2024-04-03 16:18:59 +0100

    integrar projeto React

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const uuid = require('uuid')
+const express = require("express");
+const uuid = require("uuid");
+import cors from "cors";
 
-const port = 3000
-const app = express()
-app.use(express.json())
+const port = 3001;
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 /*
 - Query params => meusite.com/users?nome=georgia&age=28 // FILTROS
@@ -17,80 +19,61 @@ app.use(express.json())
 
 Middleware => INTERCEPTADOR => Tem o poder de parar ou alterar dados da requisição
 */
- const users = []
 
-const checkUserId = (request, response, next) => {  //Middleware
-    
-    const { id } = request.params
+const users = [];
 
-    const index = users.findIndex(user => user.id === id)
+const checkUserId = (request, response, next) => {
+  //Middleware
 
-    if(index < 0){
-        return response.status(404).json({erro: "User not found"})
-    }
+  const { id } = request.params;
 
-request.userIndex = index
-request.userId = id
+  const index = users.findIndex((user) => user.id === id);
 
-next()
+  if (index < 0) {
+    return response.status(404).json({ erro: "User not found" });
+  }
 
-}
+  request.userIndex = index;
+  request.userId = id;
 
+  next();
+};
 
+app.get("/users", (request, response) => {
+  return response.json(users);
+});
 
- app.get('/users', (request, response) =>{
+app.post("/users", (request, response) => {
+  const { name, age } = request.body;
 
+  const user = { id: uuid.v4(), name, age };
 
-   return response.json(users)
-})
+  users.push(user);
 
+  return response.status(201).json(user);
+});
 
-app.post('/users', (request, response) =>{
-const {name, age} = request.body
+app.put("/users/:id", checkUserId, (request, response) => {
+  const { name, age } = request.body;
+  const index = request.userIndex;
 
-const user = {id:uuid.v4(), name, age }
+  const id = request.userId;
 
-   users.push(user) 
+  const updateUser = { id, name, age };
 
-   return response.status(201).json(user)
- })
+  users[index] = updateUser;
 
- app.put('/users/:id', checkUserId, (request, response) =>{
+  return response.json(updateUser);
+});
 
-    const { name, age } = request.body
-const index = request.userIndex
+app.delete("/users/:id", checkUserId, (request, response) => {
+  const index = request.userIndex;
 
-const id = request.userId
-   
-const updateUser = { id, name, age }
+  users.splice(index, 1);
 
-
-    users[index] = updateUser
-
-    return response.json(updateUser)
- })
-
- app.delete('/users/:id',checkUserId, (request, response) =>{
-    const index = request.userIndex
-
-users.splice(index,1)
-
-return response.status(204).json()
- })
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return response.status(204).json();
+});
 
 app.listen(port, () => {
-    console.log(`🚀 Server Started on port ${port}`)
-})
+  console.log(`🚀 Server Started on port ${port}`);
+});
